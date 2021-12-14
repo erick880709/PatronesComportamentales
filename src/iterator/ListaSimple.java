85f526b7a7d123770f3fdaa8b874868f560c1aea
erick880709 <erick880709@hotmail.com>
2021-12-13 19:31:18 -0500

    patrones de diseño
    ejemplos de patrones de diseño, codigo java

---
+++ src/iterator/ListaSimple.java
@@ -0,0 +1,12 @@
+package iterator;
+
+public interface ListaSimple {
+
+    public int count();
+
+    public boolean append(Object o);
+
+    public boolean remove(Object o);
+
+    public Object get(int index);
+}
